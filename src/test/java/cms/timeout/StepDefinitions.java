156b0f92984b203d5617f52654735246e253bbde
srikanthgoudm <srikanth2884@gmail.com>
2014-08-26 14:03:56 +0100

    updated by Sree

--- src/test/java/cms/timeout/StepDefinitions.java
+++ src/test/java/cms/timeout/StepDefinitions.java
@@ -6,11 +6,18 @@ import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 import cucumber.table.DataTable;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.net.URL;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -19,12 +26,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class StepDefinitions extends BaseClass{
 
-    //WebDriver instance variable
-    // static WebDriver driver;
-
-    //Data
-    String username = "srikanth", password = "Sairam99";
+    // Test Data -----------------------------------------------
+    String username = "autotest-admin", password = "outtime99";
     String random= String.valueOf(new Random().nextInt());
+
+
+    //-------------------------------------------------------------------------------------------
     public static boolean isTextPresent(String text)
     {
         return getVisibleText().contains(text);
@@ -58,47 +65,50 @@ public class StepDefinitions extends BaseClass{
             System.out.println("Invalid Credentials");
         }
     }
-
+//BrowserFactory----------------------------------
+// WeDriver or Remote WebDriver Initialisations
     @Before
     public void setUp()throws NullPointerException{
-        driver = new FirefoxDriver();
-        driver.get("http://admin.qa04.d/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+//        driver = new FirefoxDriver();
+//        driver.get("http://admin.qa04.d/");
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
-//        try {
-//            //Creating a Firefox WebDriver Object
-////           driver = new FirefoxDriver();
-//            //Create an object for Desired Capabilities
-//            DesiredCapabilities caps = DesiredCapabilities.firefox();
-//            caps.setCapability("platform", "Windows 8");
-//            caps.setCapability("version", "3.0");
+        try {
+            //Creating a Firefox WebDriver Object
+//           driver = new FirefoxDriver();
+            //Create an object for Desired Capabilities
+            DesiredCapabilities caps = DesiredCapabilities.firefox();
+            caps.setCapability("platform", "Windows 8");
+            caps.setCapability("version", "3.0");
+
+            // Create the connection to Sauce Labs to run the tests
+            driver = new RemoteWebDriver(
+            new URL("http://timeoutdigital:b6315b1b-3640-4a38-aa72-54c4fa2ca570@ondemand.saucelabs.com:80/wd/hub"),caps);
+            //Open the URL
+            driver.get("http://admin.qa04.d/");
+            //Maximising the window
+            driver.manage().window().maximize();
+            //waiting for page to load
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //
-//            // Create the connection to Sauce Labs to run the tests
-//            driver = new RemoteWebDriver(
-//            new URL("http://timeoutdigital:b6315b1b-3640-4a38-aa72-54c4fa2ca570@ondemand.saucelabs.com:80/wd/hub"),caps);
-//            //Open the URL
-//            driver.get("http://admin.qa04.d/");
-//            //Maximising the window
-//            driver.manage().window().maximize();
-//            //waiting for page to load
-//            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
     @After
     public void stop() {
-       // driver.quit();
+     driver.quit();
     }
+
+    //------------LOGIN------------------
     @Given("^I am Logged-In$")
     public void I_am_Logged_In() {
     this.login(username,password);
     driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
     Assert.assertTrue(driver.findElement(By.linkText(username)).isDisplayed());
     }
-
+    //------------------ADD Venue-----------------------------
     @When("^I add a Venue$")
     public void I_add_a_Venue() throws InterruptedException {
         driver.findElement(By.linkText("Dashboard")).isDisplayed();
@@ -122,7 +132,8 @@ public class StepDefinitions extends BaseClass{
        // DataTable dataTable = DataTable.create(raw, Locale.getDefault(),"Language", "Name", "City", "Site");
         this.selectFromDropDownMenu(By.id("venueCreate_language"),raw.get(0));
         try {
-            driver.findElement(By.id("venueCreate_name")).sendKeys(raw.get(1)+random);
+            String a= raw.get(1)+random;
+            driver.findElement(By.id("venueCreate_name")).sendKeys(a);
         }catch (Exception e)
         {
             System.out.println("We have found some similar sounding venues, please review them below before saving this venue ");
@@ -144,14 +155,49 @@ public class StepDefinitions extends BaseClass{
         Assert.assertTrue(this.isTextPresent(message));
     }
 
-    @Then("^it should be navigate to the 'Edit venue' Page$")
-    public void it_should_be_navigate_to_the_Edit_venue_Page() {
-//    Assert.assertTrue(this.isTextPresent(editVenuePage));
+    @Then("^it should be navigate to the '(.*)' Page$")
+    public void it_should_be_navigate_to_the_Edit_venue_Page(String editVenuePage) {
+   Assert.assertTrue(this.isTextPresent(editVenuePage));
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.linkText("Logout")).click();
+//        driver.findElement(By.linkText("Logout")).click();
     }
 
-// Edit Venue
+//--------------ADD Taxonomy---------------------
+
+    @When("^I add taxonomy for the venue$")
+    public void taxonomy()
+    {
+        //driver.findElement(By.linkText("Taxonomy")).click();
+        driver.findElement(By.xpath(".//*[@id='column2']/ul/li[2]/a")).click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id='tagger_1']/div[1]/ul/li[4]/div")).click();
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//div[@id='tagger_1']/div[1]/ul/li[4]/ul/li[4]/span"))).doubleClick().build().perform();
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);WebElement element = driver.findElement(By.xpath("//div[@id='tagger_1']/div[2]/ul/li[4]/ul/li[4]/span"));
+
+        WebElement target = driver.findElement(By.xpath("//div[@id='primaryTag']/span"));
+
+        (new Actions(driver)).dragAndDrop(element, target).perform();
+//        driver.manage().timeouts().implicitlyWait(50,TimeUnit.SECONDS);
+    }
+
+@When("^I go back to Edit Venue Page$")
+    public void backToVenue()
+    {
+      driver.findElement(By.linkText("Edit Venue")).click();
+    }
+//--------------LOGOUT-------------------------------------
+    @When("^I logout$")
+public void I_logout()
+{
+    driver.findElement(By.linkText("Logout")).click();
+}
+@Then("^I should redirect to Login Page$")
+public void backtologinpage()
+{
+    Assert.assertEquals("Login", driver.findElement(By.xpath(".//*[@id='content']/h1")).getText());
+}
+// -----------------Edit Venue--------------------------------------
 @Given("^I am on the Venues Page$")
 public void I_am_on_the_Venues_Page() {
     this.navigateToVenuesPage();
@@ -159,16 +205,15 @@ public void I_am_on_the_Venues_Page() {
     Assert.assertTrue(this.isTextPresent("Venues"));
 
 }
-
     @When("^I search for the venue with the Name as '(.*)' and Site as '(.*)'$")
     public void I_search_for_the_venue_with_the_Name_as_Srikanth_and_Site_as_UK_London(String name,String site) {
         driver.findElement(By.id("venue_filter_name")).sendKeys(name);
-        this.selectFromDropDownMenu(By.id("venue_filter_site"),site);
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        this.selectFromDropDownMenu(By.id("venue_filter_site"), site);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
     }
     @When("^I selects UpdatedInLast as '(.*)'$")
-    public void I_selects_UpdatedInLast_as_24_Hours(String UpdatedInLast)
+    public void I_selects_UpdatedInLast(String UpdatedInLast)
     {
         this.selectFromDropDownMenu(By.id("venue_filter_updated_last"),UpdatedInLast);
 //        this.selectWithIndex(By.id("venue_filter_updated_last"),index);
@@ -180,23 +225,21 @@ public void I_am_on_the_Venues_Page() {
     public void I_select_the_recently_created_Venue_with_the_name(String name) {
     driver.findElement(By.partialLinkText(name)).click();
     driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-
     }
 
     @Then("^I should navigate to the Edit Venue Page$")
     public void I_should_navigate_to_the_Edit_Venue_Page() {
-
-
     }
 
     @When("^I changes the BuildingNo as '(.*)' and Author as '(.*)' and Status as '(.*)'$")
-    public void I_changes_the_BuildingNo_as_Sri_Building_and_Author_as_Srikanth_and_Status_as_Completed(String BuildingNo,String Author,String Status) {
+    public void I_changes_the_BuildingNo_as_Sri_Building_and_Author_as_Srikanth_and_Status_as_Completed(String BuildingNo,String Author,String Status) throws InterruptedException {
     driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
         driver.findElement(By.id("venueEdit_building_no")).clear();
         driver.findElement(By.id("venueEdit_building_no")).sendKeys(BuildingNo);
         driver.findElement(By.id("venueEdit_author")).clear();
         driver.findElement(By.id("venueEdit_author")).sendKeys(Author);
-    this.selectFromDropDownMenu(By.id("venueEdit_status"),Status);
+    this.selectFromDropDownMenu(By.id("venueEdit_status"), Status);
+        Thread.sleep(3000);
     }
 
     @When("^I save the Venue$")
